a3277fe69a410c73cf4962af5540dbf5fbdbd953
nobusugi246 <nobusugi246@gmail.com>
2018-02-11 11:16:35 +0900

    initial commit.

---
+++ proto/src/main/java/proto/proto/ProtoApplication.java
@@ -0,0 +1,12 @@
+package proto.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoApplication.class, args);
+	}
+}
---
+++ proto/src/test/java/proto/proto/ProtoApplicationTests.java
@@ -0,0 +1,16 @@
+package proto.proto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
